f7465c4f22fa7836b62561657047702846aa9c4f
Jenny Ngo <jennyngo.dev@gmail.com>
2016-07-07 11:47:47 -0400

    added responsive nav

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,13 @@
-//compile things 
+$(function() {
+    $('.fa-bars').on('click', function(){
+    	$('.main-nav-links').addClass('show-responsive-nav');
+    });
+
+    $('.fa-times').on('click', function(){
+    	$('.main-nav-links').removeClass('show-responsive-nav')
+    });
+
+    $('.main-nav-links a').on('click', function(){
+    	$('.main-nav-links').removeClass('show-responsive-nav')
+    });
+}); 
--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,15 @@
-//compile things
-"use strict";
+'use strict';
+
+$(function () {
+    $('.fa-bars').on('click', function () {
+        $('.main-nav-links').addClass('show-responsive-nav');
+    });
+
+    $('.fa-times').on('click', function () {
+        $('.main-nav-links').removeClass('show-responsive-nav');
+    });
+
+    $('.main-nav-links a').on('click', function () {
+        $('.main-nav-links').removeClass('show-responsive-nav');
+    });
+});
